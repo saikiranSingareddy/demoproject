3caf9905bdabe696af89fc635df1c3cf5eb03890
saikiran reddy <saikiran199@gmail.com>
2021-10-18 09:24:45 +0530

    adding demo app

---
+++ demoapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse("this is first app")
+
+def demo(request):
+    return HttpResponse("Thank you")
